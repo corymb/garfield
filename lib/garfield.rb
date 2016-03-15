024fdae3a011ccdad0e1e2f0c96f6b37e583094d
Dave Bush <github@david-bush.co.uk>
2016-03-15 18:46:38 +0000

    Added working version.

--- lib/garfield.rb
+++ lib/garfield.rb
@@ -1,4 +1,5 @@
 require "garfield/version"
+require 'garfield/cli'
 
 module Garfield
   # Your code goes here...
---
+++ lib/garfield/cli.rb
@@ -0,0 +1,22 @@
+require 'thor'
+
+module Garfield
+  class GuardfileGenerator < Thor
+    desc 'hello', 'this will greet yowwww'
+    long_desc <<-HELLO_WORLD
+
+    `hello NAME` will print out a message to the person of your choosing.
+
+    Brian Kernighan actually wrote the first "Hello, World!" program
+    as part of the documentation for the BCPL programming language
+    developed by Martin Richards. BCPL was used while C was being
+    developed at Bell Labs a few years before the publication of
+    Kernighan and Ritchie's C book in 1972.
+
+    http://stackoverflow.com/a/12785204
+    HELLO_WORLD
+    def generate()
+      puts 'What upppppp.'
+    end
+  end
+end
