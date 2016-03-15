3fe3a782660725ad4401d8a16979b7fcf2b864be
Dave Bush <github@david-bush.co.uk>
2016-03-15 22:40:09 +0000

    Added output when generating Guardfile.

--- lib/garfield/cli.rb
+++ lib/garfield/cli.rb
@@ -5,8 +5,10 @@ module Garfield
     desc 'generate', 'Generates Guardfile'
     def generate()
       extension = Garfield.get_file_count('rb') < Garfield.get_file_count('py') ? 'py' : 'rb'
+      puts "Writing Guardfile to monitor #{extension} files..."
       test_string = Garfield.get_test_string(extension)
       Garfield.write_guardfile(extension, test_string)
+      puts 'Complete!'
     end
   end
 end
